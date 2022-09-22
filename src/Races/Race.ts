ae74dd653c3a6e70f61eda1dc7f8470a36d7a4ae
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-09-22 14:58:54 -0300

    feat: método estático createdracesInstances que retorna um number, mas na classe Race ele lança um erro

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -7,11 +7,15 @@ export default class Race {
     this._dexterity = dexterity;
   }
 
-  get name() {
+  public get name(): string {
     return this._name;
   }
 
-  get dexterity() {
+  public get dexterity(): number {
     return this._dexterity;
+  }
+
+  public static createdRacesInstances(): number {
+    throw new Error('Not implemented');
   }
 }
