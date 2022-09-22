dcb891e7d68213296127b5aac100542e8e0e76f4
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-09-22 17:38:01 -0300

    feat: método estático createdArchetypeInstances que retorna um number

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -9,16 +9,20 @@ abstract class Archetype {
     this._cost = 0;
   }
 
-  public get name() {
+  public get name(): string {
     return this._name;
   }
 
-  public get special() {
+  public get special(): number {
     return this._special;
   }
 
-  public get cost() {
+  public get cost(): number {
     return this._cost;
+  }
+
+  public static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
   }
 }
 
