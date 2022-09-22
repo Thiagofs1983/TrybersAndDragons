fbb2f94d288ccfc062cfa87c71f0d49923e7f225
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-09-22 18:05:59 -0300

    feat: classe Mage extendendo da classe Archetype que implementa as funções herdadas de Archetype com o energyType mana

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,18 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Mage extends Archetype {
+  private _energyType: EnergyType = 'mana';
+  private static _count = 0;
+
+  public static createdArchetypeInstances(): number {
+    this._count += 1;
+    return this._count;
+  }
+
+  public get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
+
+export default Mage;
