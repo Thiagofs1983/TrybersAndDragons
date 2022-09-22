ecb7cc24d78d31f556e1c785ab1733a49732730a
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-09-22 17:28:41 -0300

    feat: classe abstrata Archetype que recebe name como parâmetro e inicia os atributos privados special e cost com o valor 0

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,13 @@
+abstract class Archetype {
+  private _name: string;
+  private _special: number;
+  private _cost: number;
+
+  constructor(name: string) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
+}
+
+export default Archetype;
