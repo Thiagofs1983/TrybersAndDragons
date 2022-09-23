e3e90cb58147a2eea76c8122602194f3a63a7647
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-09-23 17:50:31 -0300

    fix: correção de bug na classe character

--- src/Character.ts
+++ src/Character.ts
@@ -62,7 +62,9 @@ class Character implements Fighter {
 
   public receiveDamage(attackPoints: number): number {
     const damage = attackPoints - this._defense;
-    this._lifePoints -= damage;
+    if (damage > 0) {
+      this._lifePoints -= damage;
+    }
     if (this._lifePoints <= 0) {
       this._lifePoints = -1;
     }
